b7457755b9310c701bf553fda447649d788639e4
菘菘 <rrt467778@gmail.com>
2024-02-13 22:03:12 +0800

    feat: gets all translations for the given text entry

--- api/src/entry/text.rs
+++ api/src/entry/text.rs
@@ -1,12 +1,12 @@
 use actix_web::{
     error::{ErrorInternalServerError, ErrorNotFound},
-    post, web,
+    get, post, web,
 };
 use log::warn;
 use serde::Deserialize;
 
 use crate::AppState;
-use service::entry::translation;
+use service::entry::translation::{self, TextTranslation};
 
 #[derive(Debug, Deserialize)]
 pub struct TranslateTextPayload {
@@ -46,11 +46,26 @@ pub async fn translate_text(
     }
 }
 
-// #[get("/{text_entry_key}/translations")]
-// pub async fn get_translations(
-//     app_state: web::Data<AppState>,
-//     text_entry_key: web::Path<String>,
-// ) -> actix_web::Result<web::Json<Vec<TextTranslation>>> {
-// }
-// }
-// }
+#[get("/{entry_key}/translations")]
+pub async fn get_translations(
+    app_state: web::Data<AppState>,
+    entry_key: web::Path<String>,
+) -> actix_web::Result<web::Json<Vec<TextTranslation>>> {
+    let entry = translation::get_text_entry(&app_state.db, entry_key.to_string())
+        .await
+        .ok_or_else(|| ErrorNotFound("Text Entry not found"))?;
+
+    let result = translation::get_translations(&app_state.db, entry).await;
+    match result {
+        Ok(translations) => Ok(web::Json(translations)),
+        Err(e) => {
+            warn!(
+                "Failed to lookup translations for the text entry whose key is {}: {:?}",
+                entry_key, e
+            );
+            Err(ErrorInternalServerError(
+                "Failed to lookup translations for the text entry.",
+            ))
+        }
+    }
+}
--- service/src/entry/translation.rs
+++ service/src/entry/translation.rs
@@ -1,13 +1,24 @@
 use entity::{
     entry::text_entry,
-    translation::{text_translation, translation_flag::TranslationFlagVec},
+    translation::{
+        text_translation,
+        translation_flag::{TranslationFlag, TranslationFlagVec},
+    },
 };
-use sea_orm::{ActiveModelTrait, ActiveValue::NotSet, DatabaseConnection, DbErr, EntityTrait, Set};
+use sea_orm::{
+    ActiveModelTrait, ActiveValue::NotSet, DatabaseConnection, DbErr, EntityTrait, ModelTrait, Set,
+};
+use serde::Serialize;
 
 pub async fn get_text_entry(db: &DatabaseConnection, key: String) -> Option<text_entry::Model> {
     text_entry::Entity::find_by_id(key).one(db).await.ok()?
 }
 
+/// Add a translated text for the text entry with the given key.
+///
+/// # Errors
+///
+/// If the translation model cannot be inserted into the database, a [`DbErr`] will be thrown.
 pub async fn add_translation(
     db: &DatabaseConnection,
     text_entry_key: String,
@@ -22,4 +33,35 @@ pub async fn add_translation(
     let result = model.insert(db).await?;
 
     Ok(result)
+}
+
+#[derive(Debug, Serialize)]
+pub struct TextTranslation {
+    pub id: i32,
+    pub content: String,
+    pub flags: Vec<TranslationFlag>,
+}
+
+/// Gets all translations for the given text entry.
+///
+/// # Errors
+///
+/// Returns a [`DbErr`] if there is an error querying the database.
+pub async fn get_translations(
+    db: &DatabaseConnection,
+    text_entry: text_entry::Model,
+) -> Result<Vec<TextTranslation>, DbErr> {
+    let translations = text_entry
+        .find_related(text_translation::Entity)
+        .all(db)
+        .await?
+        .into_iter()
+        .map(|model| TextTranslation {
+            id: model.id,
+            content: model.content,
+            flags: vec![],
+        })
+        .collect();
+
+    Ok(translations)
 }
